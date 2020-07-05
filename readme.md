fb1623f87a7626eadf78803a5b93543979aafc4c
rfischbacher <64086287+rfischbacher@users.noreply.github.com>
2020-07-06 00:45:48 +0200

    Commit code 5 juillet

--- readme.md
+++ readme.md
@@ -1,11 +1,17 @@
-# Visualisation de la température journalière à Genève au cours du temps
+# Visualisation de la température journalière à Genève au cours d'une année
 
-Il s'agit d'un projet consacré à la réalisation d'une visualisation en utilisant la librairie JavaScript D3.js qui permet de manipuler des données.
-J'ai choisi de travailler sur la température moyenne journalière de l'air à Genève pour visualiser son évolution au cours des années.
-Les données proviennent de MétéoSuisse. Elles sont téléchargeable sur le lien opendata.swiss suivant:
+Il s'agit d'un projet consacré à la réalisation d'une visualisation en utilisant la librairie JavaScript D3.js qui permet de manipuler des données. Ce travail a été réalisé dans le cadre du cours "Visualisation de données", donné par Loïc Cattani à l'UNIL en 2020. Mon domaine d'étude étant les géosciences, j'ai choisi ici de travailler sur la température moyenne journalière de l'air à Genève pour visualiser son évolution au cours du temps. L'objectif de ce travail aura été de visualiser ces données sur une page HTML en utilisant le langage JavaScript.
+
+Lien vers la visualisation:
+.....
+
+SELECTION DES DONNEES
+Les données proviennent de MétéoSuisse. Elles sont téléchargeables sur le lien opendata.swiss suivant:
 https://opendata.swiss/fr/dataset/klimamessnetz-tageswerte
+Le fichier csv initial contient les données pour plusieurs stations suisses et couvre la période de 1950 à 2019. Pour chaque année, on y trouve notamment la température moyenne mesurée chaque jour à 2 mètres au-dessus du sol. Pour ne pas surcharger la visualisation, j'ai uniquement sélectionné comme exemple la température journalière à Genève durant l'année 2012 et 2019. Ce sont deux années intéressantes à étudier d'un point de vue météorologique, notamment avec l'hiver 2012 particulièrement froid et l'été 2019 très chaud.
 
-Le fichier csv contient les données pour plusieurs stations suisse et couvre la période de 1950 à 2019.
-Pour chaque année, on y trouve notamment la température moyenne mesurée chaque jour à 2 mètres au dessus du sol.
+VISUALISATION
+Les données sont visualisées sur un graphique, avec l'axe x représentant chaque jour de l'année (allant de 1 pour le 1er janvier à 365 pour le 31 décembre). L'axe y représente la température moyenne mesurée par la station pour une journée donnée. Les données de chaque jour sont reliées entre elles par une courbe lissée pour visualiser les variations de température au cours d'une année. La courbe de 2012 est représentée en bleu et celle de 2019 en rouge. Ces deux années ont été choisies pour être comparée l'une à l'autre en les superposant sur le même graphique.
 
-Les données sont visualisée sur un graphique, avec l'axe x représentant chaque jour de l'année (001 pour le 1er janvier et 365 pour le 31 décembre). L'axe y représente la température moyenne mesurée par la station pour une journée donnée. Les données de chaque jour sont reliées entre elles pour visualiser les variation de température au cours d'une année.
+
+En tant que novice, ce projet aura été une bonne introduction à la programmation en JavaScript, à la création de pages HTML et la mise en forme avec CSS. J'ai également appris à utiliser deux nouveaux outils particulièrement pratiques, qui sont Visual Studio Code pour coder et GitHub pour héberger les scripts et enregistrer leur évolution.
--- scripts/main.js
+++ scripts/main.js
@@ -1,65 +1,106 @@
+// Code main.JS permettant la visualisation des données du fichier GVE_2010_2019.CSV sur la page index.HTML
+// Réalisé par Romain Fischbacher - Juillet 2020
+
 // Dimensions et marges du graphique SVG
 const margin = { top: 50, right: 50, bottom: 50, left: 50 };
 const width = 1000 - margin.left - margin.right;
-const height = 1000 - margin.top - margin.bottom;
+const height = 750 - margin.top - margin.bottom;
 
-// Lecture du fichier csv et création de clés-valeurs pour chaque colonne de données
-d3.dsv(';', 'data/GVE_1950_2019.csv', function (d) {
-    return{
-        station: d.stn,
-        year: parseFloat(d.time.substr(0, 4)),
-        month: parseFloat(d.time.substr(4, 2)),
-        day: parseFloat(d.time.substr(6, 2)),
-        temp: parseFloat(d.tre200d0)
-    }
-}).then(function(data) {   // début de la fonction
+// Créer l'élément SVG et le configurer
+const svg = d3.select('.main')
+              .append('svg')
+                  .attr('width', width + margin.left + margin.right)
+                  .attr('height', height + margin.top + margin.bottom)
+                  .attr('style', 'font: 10px sans-serif')
+              .append("g")
+                  .attr("transform",
+                        "translate(" + margin.left + "," + margin.top + ")");
 
-// Filtrer notre station de Genève pour une année donnée
-const station = data.filter(d => d.year === 2019);
+// Lecture du fichier CSV et création de clés-valeurs pour toutes les colonnes de données
+d3.csv('data/GVE_2010_2019.csv', function (d){
+    return{ Jour : parseFloat(d.time),
+            Temp_2010 : parseFloat(d.temp_2010),
+            Temp_2011 : parseFloat(d.temp_2011),
+            Temp_2012 : parseFloat(d.temp_2012),
+            Temp_2013 : parseFloat(d.temp_2013),
+            Temp_2014 : parseFloat(d.temp_2014),
+            Temp_2015 : parseFloat(d.temp_2015),
+            Temp_2016 : parseFloat(d.temp_2016),
+            Temp_2017 : parseFloat(d.temp_2017),
+            Temp_2018 : parseFloat(d.temp_2018),
+            Temp_2019 : parseFloat(d.temp_2019)
+        }
+}).then(function(data) {   // Promesse (traitement asynchrone des données)
+    
+    // DEBUT DE LA FONCTION
 
-//console.log(station); // voir les données séléctionnées
+    // Affichage des données séléctionnées dans la console de la page HTML
+    console.log(data);
+  
+    // Axe x pour les jours (numéro du jour de l'année de 1-365)
+    const x = d3.scaleLinear()
+                .domain(d3.extent(data, function(d) { return d.Jour; }))
+                .range([ 0, width ]);
+            svg.append("g")
+                .attr("class", "axis")
+                .attr("transform", "translate(0," + height + ")")
+                .call(d3.axisBottom(x));
+  
+    // Axe y pour la température mesurée (valeur en degré Celsius)
+    const y = d3.scaleLinear()
+                //.domain([-10,30])
+                .domain([d3.min(data, function(d) { return Math.min(d.Temp_2012,d.Temp_2019); }), d3.max(data, function(d) { return Math.max(d.Temp_2012,d.Temp_2019); })])
+                .range([ height, 0 ]);
+            svg.append("g")
+                .attr("class", "axis")
+                .call(d3.axisLeft(y));
+  
+    // Création d'une courbe lissée pour 2012
+    var valueline2012= d3.line()
+        .x(function(d) { return x(d.Jour); })
+        .y(function(d) { return y(d.Temp_2012); })
+        .curve(d3.curveBasis);      
+    // Création d'une courbe lissée pour 2019
+    var valueline2019 = d3.line()
+        .x(function(d) { return x(d.Jour); })
+        .y(function(d) { return y(d.Temp_2019); })
+        .curve(d3.curveBasis);                              
 
-  // Créer l'élément SVG et le configurer
-  const svg = d3.select('.main')
-                .append('svg')
-                .attr('width', width + margin.left + margin.right)
-                .attr('height', height + margin.top + margin.bottom)
-                .attr('style', 'font: 10px sans-serif')
-  
-  // Axe x pour le temps (numéro du jour dans l'année)
-  const x = d3.scaleTime()
-              .domain([0, d3.max(station, function(d) { return d.day; })])
-              .range([0, width])
-  
-  // Axe y pour la température (valeur en degré)
-  const y = d3.scaleLinear()
-              .domain([0, d3.max(station, function(d) { return d.temp; })])
-              .range([height, 0])
-  
-  // definir une ligne
-  var ligne = d3.line()
-                .x(function(d) { return d.day; })
-                .y(function(d) { return d.temp; })
-                .curve(d3.curveMonotoneX);            
-  
-  // Relier les valeures de température par une ligne
-  svg.append("path")
-      .datum(station)
-      .attr("class", "line")
-      .attr("d", ligne)
-  
-  // Faire les axes
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
+    // Relier les données pour 2012
+    svg.append("path")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "#0000FF")
+        .attr("stroke-width", 3)
+        .attr("d", valueline2012); 
+    // Relier les données pour 2019
+    svg.append("path")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "#FE2E2E")
+        .attr("stroke-width", 3)
+        .attr("d", valueline2019);                                               
+            
+    // Titre de l'axe x
+    svg.append("text") 
+        .attr('class', 'axis-label')
+        .attr("x", width /2)
+        .attr("y", height + 40)
+        .style("text-anchor", "middle")
+        .text("Temps [jours]");  
 
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(d3.axisLeft(y));
+    // Titre de l'axe y
+    svg.append("text")
+        .attr('class', 'axis-label')
+        .attr("transform", "rotate(-90)")
+        .attr("text-anchor", "middle") 
+        .attr("y", 0 - margin.left)
+        .attr("x",0 - (height / 2))
+        .attr("dy", "1em")
+        .style("text-anchor", "middle")
+        .text("Température [°C]");  
+    
+    // Légende des courbes    
 
-  const yaxis = d3.axisLeft().scale(x); 
-  const xaxis = d3.axisBottom().scale(y);
-  
-  
-}) // fin de la fonction
+    
+}) // FIN DE LA FONCTION
